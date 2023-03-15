52e66871c026d7158accd815e11313fdf71f9652
Jan Dostal <jan.dostal@tul.cz>
2023-03-15 19:13:51 +0100

    Made progress in exercise two

--- exercise_02/gauss_elim.m
+++ exercise_02/gauss_elim.m
@@ -1,19 +1,13 @@
 function [x, U] = gauss_elim(A, b)
     
+    % transponovat vektor b, pokud spatny format
+    
     numberOfRows = size(A, 1);
     numberOfCols = size(A, 2);
 
     for diagonalLevel = 1:numberOfRows-1
+       
         
-        %test na singularitu
-        [~, U, ~] = lu(A);
-        determinant = prod(diag(U));
-
-        if determinant == 0
-        
-            error('singular');
-        end
-
         % pivotace
         [~, controlElementIndex] = max(abs(A(diagonalLevel:end, diagonalLevel)));
     
@@ -45,5 +39,40 @@ function [x, U] = gauss_elim(A, b)
         end
     end
 
+    %test na singularitu
+    [~, U, ~] = lu(A);
+    determinant = prod(diag(U));
+
+    if determinant == 0
+        
+        error('singular');
+    end
+
     U = A;
+
+    %zpetny chod
+
+    for diagonalLevel = numberOfRows:-1:2
+        
+        % nasobeni a scitani radku
+        for rowLevel = diagonalLevel-1:-1:1
+        
+            if A(rowLevel, diagonalLevel) == 0
+            
+                continue;
+            end
+
+            for colLevel = diagonalLevel:-1:1
+            
+                A(rowLevel, colLevel) = A(diagonalLevel, colLevel) * ...
+                    (- A(rowLevel, diagonalLevel) / A(diagonalLevel, diagonalLevel)) ...
+                    + A(rowLevel, colLevel);
+            end
+
+            b(rowLevel) = b(diagonalLevel) * ...
+                (- A(rowLevel, diagonalLevel) / A(diagonalLevel, diagonalLevel)) ...
+                + b(rowLevel);
+        end
+    end
+    end
 end
