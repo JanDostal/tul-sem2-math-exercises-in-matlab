0c0fdc619797746ab1ae6ce830b07d9d9aaf4ca0
Jan Dostal <jan.dostal@tul.cz>
2023-04-03 20:18:09 +0200

    Init forth exercise

---
+++ exercise_04/mocninna_metoda.m
@@ -0,0 +1,62 @@
+function [vec, lambda] = mocninna_metoda(A, y0, tol, maxit)
+    
+    if size(A, 1) <= 1 || size(A, 2) <= 1
+        
+        error('input A not matrix');
+    end
+
+    if (size(y0, 1) <= 1 && size(y0, 2) <= 1) || (size(y0, 1) > 1 && size(y0, 2) > 1)
+        
+        error('input b not vector');
+    end
+
+    if size(y0, 2) ~= 1
+        
+        error('input b not column vector');
+    end
+
+    if size(A, 1) ~= size(A, 2)
+        
+        error("input A not square matrix");
+    end
+
+    if size(A, 1) ~= size(A, 2)
+        
+        error("input A not square matrix");
+    end
+
+    if length(y0) ~= size(A, 1)
+
+        error("input b not same length as matrix");
+    end
+    
+    if isempty(tol) || size(tol, 1) > 1 || size(tol, 2) > 1 || tol <= 0
+        
+        tol = 1e-6;
+    end
+
+    if isempty(maxit) || size(maxit, 1) > 1 || size(maxit, 2) > 1 || maxit <= 0
+        
+        maxit = 1000;
+    end
+    
+    normalizedY0 = y0 / norm(y0);
+    y0_temp = normalizedY0;
+
+    for i = 1:maxit
+
+        y0 = A * normalizedY0;
+        maxElement = max(abs(y0));
+        normalizedY0 = y0 / maxElement;
+
+        if norm(normalizedY0 - y0_temp) < tol
+
+            break;
+        end
+
+        y0_temp = normalizedY0;
+    end
+
+    vec = normalizedY0;
+    lambda = maxElement;
+end
