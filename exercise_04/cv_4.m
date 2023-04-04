4143562c4371790dbdceaa4adaaad241faa8074a
Jan Dostal <jan.dostal@tul.cz>
2023-04-04 02:28:00 +0200

    Completed forth exercise

--- exercise_04/cv_4.m
+++ exercise_04/cv_4.m
@@ -1,3 +1,25 @@
+testMatrix1 = [1 1 0; 1 1 1; 0 1 1];
+testMatrix2 = [1.5 -6.5 6.5; 1 -6 6.5; 1 -1 1.5];
+
+[v1, l1] = mocninna_metoda(testMatrix1, [1; 1; 1], 1e-15, 1500);
+[v2, l2] = mocninna_metoda(testMatrix2, [1; 1; 1], 1e-15, 1500);
+
+tM11 = v1*l1;
+tM12 = testMatrix1*v1;
+
+tM21 = v2*l2;
+tM22 = testMatrix2*v2;
+
+[v11, l11] = eig(testMatrix1);
+[l11, index1] = max(diag(l11));
+v11 = v11(:, index1);
+
+[v21, l21] = eig(testMatrix2);
+[l21, index2] = max(diag(l21));
+v21 = v21(:, index2);
+
+
+
 webPagesLinks = [0 1 1 1 1;
                  1 0 1 0 0;     
                  1 1 0 0 1;     
@@ -16,14 +38,16 @@ for page = 1:webPagesNumber
     M(:, page) = webPageLinks;
 end
 
-[vec, lambda] = mocninna_metoda(M, ones(webPagesNumber, 1), 1e-6, 1000);
+[vec, lambda] = mocninna_metoda(M, [1; 1; 1; 1; 1], 1e-15, 1500);
+
+[vec2, lambda2] = eig(M);
+[lambda2, index3] = max(diag(lambda2));
+vec2 = vec2(:, index3);
 
 websNames = ['A', 'B', 'C', 'D', 'E'];
 [~, indeces] = sort(abs(vec));
 
 webs = websNames(indeces);
-
-
 
 
 
--- exercise_04/mocninna_metoda.m
+++ exercise_04/mocninna_metoda.m
@@ -20,11 +20,6 @@ function [vec, lambda] = mocninna_metoda(A, y0, tol, maxit)
         error("input A not square matrix");
     end
 
-    if size(A, 1) ~= size(A, 2)
-        
-        error("input A not square matrix");
-    end
-
     if length(y0) ~= size(A, 1)
 
         error("input b not same length as matrix");
@@ -37,23 +32,24 @@ function [vec, lambda] = mocninna_metoda(A, y0, tol, maxit)
 
     if isempty(maxit) || size(maxit, 1) > 1 || size(maxit, 2) > 1 || maxit <= 0
         
-        maxit = 100000;
+        maxit = 1000;
     end
 
     y0 = y0/norm(y0);
 
     for i = 1:maxit
-        y = A*y0;
-        lambda = y0'*y;
+
+        y = A * y0;
+        lambda = y0' * y;
         y = y/norm(y);
 
-        if norm(y0 - y) < tol
+        if norm(y - y0) < tol
+
             break;
         end
 
         y0 = y;
     end
 
-    vec = y0;
-
+    vec = y;
 end
