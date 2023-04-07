09252153d0f76443bd6dc68e5d57088caa5abe20
Jan Dostal <jan.dostal@tul.cz>
2023-04-07 19:02:36 +0200

    Completed fifth exercise

---
+++ exercise_05/cv_5.m
@@ -0,0 +1,23 @@
+roots1 = roots([1 -4 2 2 1 6]);
+[x1, M1] = find_int_roots([1 -4 2 2 1 6]);
+
+roots2 = roots([1 -3 3 -1]);
+[x2, M2] = find_int_roots([1 -3 3 -1]);
+
+roots3 = roots([1, 37]);
+[x3, M3] = find_int_roots([1, 37]);
+
+roots4 = roots([10, -120]);
+[x4, M4] = find_int_roots([10, -120]);
+
+roots5 = roots([1 4 -15 -70 -16 96]);
+[x5, M5] = find_int_roots([1 4 -15 -70 -16 96]);
+
+roots6 = roots([1 1 -3 -7 -40 -60]);
+[x6, M6] = find_int_roots([1 1 -3 -7 -40 -60]);
+
+roots7 = roots([7 7 -21 -49 -280 -420]);
+[x7, M7] = find_int_roots([7 7 -21 -49 -280 -420]);
+
+roots8 = roots([1 1 -3 -7 -40 -60 0 0]);
+[x8, M8] = find_int_roots([1 1 -3 -7 -40 -60 0 0]);
--- exercise_05/find_int_roots.m
+++ exercise_05/find_int_roots.m
@@ -1,45 +1,95 @@
 function [x, M] = find_int_roots(p)
- n = length(p) - 1; 
-x = [];
-M = []; 
 
-candidates = [];
-if p(1) == 0
-    candidates = [candidates, 0];
-end
-for i = 1:abs(p(n+1))
-    if mod(p(n+1), i) == 0
-        candidates = [candidates, i, -i];
+    %ocekavany prvni prvek p jako nejvyssi mocnina
+
+    if isempty(p) || (size(p, 1) > 1 && size(p, 2) > 1)
+        
+        error('input p not vector or scalar');
     end
-end
 
-
-A = max(abs(p(1:n))) / abs(p(n+1));
-lb = -1 - A/abs(p(n+1));
-ub = 1 + A/abs(p(n+1));
-discr = length(find(p(2:end) .* p(1:end-1) < 0)) - 2*mod(n-1, 2);
-for i = floor(lb):ceil(ub)
-    if sum(p.*i.^[n:-1:0]) == 0
-        candidates = [candidates, i];
+    if size(p, 1) == 1 && size(p, 2) == 1
+        
+        x = [];
+        M = [];
+        return;
     end
-end
+    
+    %veta o omezenosti korenu
 
+    flippedP = flip(p);
+    n = length(p);
+    A = max(abs(flippedP(1:n-1)));
+    upperBound = 1 + A/abs(flippedP(n));
+    lowerBound = -upperBound;
 
-for i = 1:length(candidates)
-    k = candidates(i);
-    q = p;
-    r = q(1);
-    M_tmp = zeros(n, 2); 
-    for j = 2:n+1
-        r = r*k + q(j);
-        q(j) = q(j) + q(j-1)*k;
-        if j <= n
-           
-            M_tmp(j-1, :) = [q(j), r];
+    if isinf(upperBound)        
+        x = [];
+        M = [];
+        return;
+    end
+
+    %veta o dělitelnosti absolutniho clenu
+
+    divisorsCandidates = divisors(flippedP(1));
+    negativeDivisors = divisorsCandidates(divisorsCandidates > 0) * -1;
+
+    if isempty(negativeDivisors) == false
+    
+        divisorsCandidates(end + 1: end + length(negativeDivisors)) = negativeDivisors;
+    end
+
+    %kombinace dělitelnosti a omezenosti
+    
+    rootsBordersInterval = [];
+
+    if lowerBound > 0
+        
+        rootsBordersInterval = upperBound:lowerBound;
+    else
+        rootsBordersInterval = lowerBound:upperBound;
+    end
+
+    candidates = divisorsCandidates(ismember(divisorsCandidates, rootsBordersInterval));
+    candidates = [candidates, rootsBordersInterval(not(ismember(rootsBordersInterval, divisorsCandidates)))];
+
+    %hornerovo schema
+    
+    candidatesCount = length(candidates);
+    polynomialDegree = length(p) - 1;
+    foundXCount = 0;
+    currentCandidateIndex = 1;
+    x = [];
+    M = zeros(1 + polynomialDegree, length(p));
+    M(1, :) = p;
+    tempM = p;
+
+    while foundXCount < polynomialDegree && currentCandidateIndex <= candidatesCount
+    
+        b = 0;
+        candidate = candidates(currentCandidateIndex);
+
+        for i = 1:length(tempM)
+        
+            b(end + 1) = b(end)*candidate + tempM(i);
         end
+
+        b = b(2:end);
+
+        if b(end) == 0
+        
+            b = b(1:end-1);
+            currentCandidateIndex = currentCandidateIndex - 1;
+            foundXCount = foundXCount + 1;
+
+            M(1 + foundXCount, 1:length(b)) = b;
+            tempM = b;
+            x(end + 1) = candidate;
+        end
+
+        currentCandidateIndex = currentCandidateIndex + 1;
     end
-    if r == 0
-        x = [x, k]
-        M = [M; M_tmp];
-    end
+    
+    x = x.';
+    M(not(any(M,2)), :) = [];
 end
+
