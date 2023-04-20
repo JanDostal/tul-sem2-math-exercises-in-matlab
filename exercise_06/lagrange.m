f0309191d2862adcf45c06095e261ecfb95a1631
Jan Dostal <jan.dostal@tul.cz>
2023-04-20 21:46:26 +0200

    Updated sixth exercise

--- exercise_06/lagrange.m
+++ exercise_06/lagrange.m
@@ -15,13 +15,13 @@ function yq = lagrange(x, y, xq)
         error('input xq not vector or scalar or matrix');
     end
 
-    if (length(y) ~= length(x) && (size(y, 1) > 1 && size(y, 2) == 1) || ...
-            (size(y, 1) == 1 && size(y, 2) > 1))
+    if (length(y) ~= length(x) && ((size(y, 1) > 1 && size(y, 2) == 1) || ...
+            (size(y, 1) == 1 && size(y, 2) > 1)))
 
         error("input x not same length as y");
     end
 
-    if (size(y, 1) ~= length(x) && (size(y, 1) > 1 && size(y, 2) > 1))
+    if size(y, 1) ~= length(x) && (size(y, 1) > 1 && size(y, 2) > 1)
 
         error("input x not same length as y1");
     end
